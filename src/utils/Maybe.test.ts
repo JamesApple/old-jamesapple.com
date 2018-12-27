6817be8cea8f182b6a1c4e96ee47dd3652ac7a38
JamesApple <JamesAppleArt@gmail.com>
2018-12-28 01:14:46 +1100

    Add jest and Maybe Monad

---
+++ src/utils/Maybe.test.ts
@@ -0,0 +1,39 @@
+import Maybe from './maybe'
+
+describe('Maybe', () => {
+  describe('.getOr', () => {
+    const testFn = (maybeString: string | null): Maybe<string> => {
+      return maybeString ? Maybe.some(maybeString) : Maybe.none()
+    }
+
+    it('It returns an existing value', () => {
+      const subject = testFn('Hello')
+      expect(subject.getOr('Bye')).toEqual('Hello')
+    })
+
+    it('It returns an existing value', () => {
+      const subject = testFn(null)
+      expect(subject.getOr('Bye')).toEqual('Bye')
+    })
+  })
+
+  describe('#map', () => {
+    const subject = Maybe.some('String')
+    it('Applies the supplied function to the contents', () => {
+      const insertMe = (s: string) => {
+        expect(s).toEqual('String')
+        return 'Success!'
+      }
+
+      expect(subject.map(insertMe).getOr('Fail')).toEqual('Success!')
+    })
+  })
+
+  describe('#fmap', () => {
+    const subject = Maybe.some('String')
+    const testFn = (_m: Maybe<string>) => Maybe.none()
+    it('Accepts a function that returns a maybe', () => {
+    expect(testFn(subject).getOr('Success!')).toEqual('Success!')
+    })
+  })
+})
---
+++ src/utils/Maybe.ts
@@ -0,0 +1,32 @@
+class Maybe<T> {
+  public static some<T>(value: T) {
+    if (!value) {
+      throw Error('Maybe.some cannot be null')
+    }
+    return new Maybe(value)
+  }
+
+  public static none<T>() {
+    return new Maybe<T>(null)
+  }
+
+  public static from<T>(value: T) {
+    return value ? Maybe.some(value) : Maybe.none<T>()
+  }
+
+  private constructor(private value: T | null) {}
+
+  public map<R>(fn: (value: T) => R): Maybe<R> {
+    return this.value == null ? Maybe.none<R>() : Maybe.some(fn(this.value))
+  }
+
+  public fmap<R>(fn: (value: T) => Maybe<R>): Maybe<R> {
+    return this.value == null ? Maybe.none<R>() : fn(this.value)
+  }
+
+  public getOr(defaultValue: T) {
+    return this.value === null ? defaultValue : this.value
+  }
+}
+
+export default Maybe
